935e71ceb20bb7d56dc3820dd441b38bf560b613
taylorsjames <t.james1017@gmail.com>
2025-11-12 08:55:36 -0600

    hey look a triangle

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from logger import log_state
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
         screen.fill((0, 0, 0))
+        player.draw(screen)
+        log_state()
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000.0  # Limit to 60 FPS
 
     print("Starting Asteroids!")
