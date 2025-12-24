a8e6395f1489fc9392c794ed5049470e05ff3ee5
taylorsjames <t.james1017@gmail.com>
2025-12-23 23:18:18 -0600

    added initial shooting mechanism

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from logger import log_event
 import sys
+from shot import Shot
 
 def main():
     pygame.init()
@@ -16,14 +17,16 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
     
     
-
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+
+    Shot.containers = (shots, updatable, drawable)
 
     asteriodfield = AsteroidField()
 
---
+++ shot.py
@@ -0,0 +1,15 @@
+from constants import *
+from circleshape import CircleShape
+import pygame
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, PLAYER_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
